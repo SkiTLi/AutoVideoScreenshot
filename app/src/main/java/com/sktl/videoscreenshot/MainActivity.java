3d19447c86943c4e8af5fe01bc47d1d81146133a
SkiTLi <kotinsandy@gmail.com>
2018-03-01 23:44:53 +0300

    v-1.3 (can get a screenshot of the video on the other thread with given fps)

--- app/src/main/java/com/sktl/videoscreenshot/MainActivity.java
+++ app/src/main/java/com/sktl/videoscreenshot/MainActivity.java
@@ -27,16 +27,16 @@ public class MainActivity extends Activity {
     private static final String VIDEO_URL = "http://ixbt.video/sadm_files/video/2015/12/CoradiaiLint.mp4";
 //    private static final String VIDEO_URL = "http://techslides.com/demos/sample-videos/small.mp4";
 
-    VideoView mVideoView;
-    ImageView mImageView;
+    private VideoView mVideoView;
+    private ImageView mImageView;
 
-    EditText mEditText;
-    List<Bitmap> bitmapList;
+    private EditText mEditText;
+    private List<Bitmap> bitmapList;
 
-    private int duration = 3000;        // 3 sec
-
-
-    MediaMetadataRetriever myMediaMetadataRetriever;
+    private int duration = 5000;        // 5 sec
+    private Thread myThread = null;
+    private int fps = 100;
+    private MediaMetadataRetriever myMediaMetadataRetriever;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,62 +59,41 @@ public class MainActivity extends Activity {
 
         MediaController controller = new MediaController(this);
         mVideoView.setMediaController(controller);
+        mVideoView.setVideoURI(Uri.parse(VIDEO_URL));
+        mVideoView.setOnPreparedListener(myVideoViewPreparedListener);
 
 
         findViewById(R.id.start_button1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                mVideoView.setMediaController(controller);
-                mVideoView.setVideoURI(Uri.parse(VIDEO_URL));
-
-                mVideoView.setOnPreparedListener(myVideoViewPreparedListener);
-
-                mVideoView.start();
-
-                bitmapList = new ArrayList<Bitmap>();
 
 
-                final Thread myThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Bitmap mBitmap;
-                        int fps = 100;
-                        if (mEditText.getText().length() != 0) {
-                            fps = Integer.parseInt(mEditText.getText().toString());
-                        }
-
-                        int dur = duration / 10 / fps;
-                        for (int i = 1; i < dur; i++) {
-
-                            mBitmap = myMediaMetadataRetriever
-                                    .getFrameAtTime(i * 10000 * fps, myMediaMetadataRetriever.OPTION_CLOSEST);
-                            Log.d("eee", "mBitmap = " + mBitmap);
-
-                            bitmapList.add(mBitmap);
-                        }
+                if (!mVideoView.isPlaying()) {
+//                    mVideoView.setOnPreparedListener(myVideoViewPreparedListener);
 
 
+                    if (mEditText.getText().length() != 0) {
+                        fps = Integer.parseInt(mEditText.getText().toString());
                     }
-                });
 
-                Log.d("eee", "myMediaMetadataRetriever.getEmbeddedPicture() = " + myMediaMetadataRetriever.getEmbeddedPicture());
-
-                myThread.start();
+                    mVideoView.start();
+                    myThread.start();
 
 
-                if (myThread.isInterrupted()) {
-                    myThread.interrupt();
                 }
+                Log.d("eee", "mVideoView.getDuration1() = " + mVideoView.getDuration());
+
+
             }
         });
 
         findViewById(R.id.shot_button3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                mImageView.setImageBitmap(bitmapList.get(bitmapList.size() - 1));
-
+                if (bitmapList.size()!= 0) {
+                    mImageView.setImageBitmap(bitmapList.get(bitmapList.size() - 1));
+                }
             }
         });
 
@@ -135,10 +114,53 @@ public class MainActivity extends Activity {
         public void onPrepared(MediaPlayer arg0) {
             duration = mVideoView.getDuration();
             Log.d("eee", "mVideoView.getDuration() = " + duration);
+
+
         }
     };
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+
+        bitmapList = new ArrayList<Bitmap>();
+
+        if (myThread == null) {
+            myThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Bitmap mBitmap;
+
+
+                    int dur = duration / 10 / fps;
+                    for (int i = 1; i < dur; i++) {
+
+                        mBitmap = myMediaMetadataRetriever
+                                .getFrameAtTime(i * 10000 * fps, myMediaMetadataRetriever.OPTION_CLOSEST);
+                        Log.d("eee", "mBitmap = " + mBitmap);
+
+                        bitmapList.add(mBitmap);
+                    }
+
+
+                }
+            });
+
+            Log.d("eee", "myMediaMetadataRetriever.getEmbeddedPicture() = " + myMediaMetadataRetriever.getEmbeddedPicture());
+
+        }
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        myThread.interrupt();
+    }
 }
 
 
